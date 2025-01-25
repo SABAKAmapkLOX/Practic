00ddc46d079a745850183e78ba12e3dfa160211c
Павел Ермаков <ermakov.pavel.111@gmail.com>
2025-01-25 12:00:19 +0300

    Добавьте файлы проекта.

---
+++ Task/StructTask.cs
@@ -0,0 +1,149 @@
+﻿namespace Task
+{
+    public struct TaskOne
+    {
+        private int threeDigitNumber;
+        private int ThreeDigitNumber
+        {
+            get
+            {
+                return threeDigitNumber;
+            }
+            set
+            {
+                if (value > 99 || value < 1000)
+                {
+                    threeDigitNumber = value;
+                }
+            }
+        }
+        public TaskOne(int threeDigitNumber)
+        {
+            ThreeDigitNumber = threeDigitNumber;
+        }
+        public bool ResultTaskOne()
+        {
+            int numberOne = threeDigitNumber % 10;
+            int numberTwo = (threeDigitNumber % 100) / 10;
+            int numberThree = threeDigitNumber / 100;
+            int numberResult = numberOne + numberTwo + numberThree;
+            if (numberResult > 10 && numberResult < 99)
+            {
+                return true; 
+            }
+            return false;
+        }
+    }
+    public struct TaskTwo
+    {
+        private int NumberOne { get; set; }
+        private int NumberTwo { get; set; }
+        private int NumberThree { get; set; }
+        public TaskTwo(int numberOne, int numberTwo, int numberThree)
+        {
+            NumberOne = numberOne;
+            NumberTwo = numberTwo;
+            NumberThree = numberThree;
+        }
+        public int MaxNumber()
+        {
+            if ((NumberOne < NumberTwo) && (NumberOne < NumberThree))
+            {
+                return NumberTwo + NumberThree;
+            }
+            if ((NumberTwo < NumberOne) && (NumberTwo < NumberThree))
+            {
+                return NumberOne + NumberThree;
+            }
+            if ((NumberThree < NumberOne) && (NumberThree < NumberTwo))
+            {
+                return NumberOne + NumberTwo;
+            }
+            else return 0;
+        }
+    }
+    public struct TaskThree
+    {
+        public int MasRow { get; set; }
+        public int MasMax { get; set; }
+        public int[] mas;
+        public TaskThree(int masMax, int masRow)
+        {
+            MasRow = masRow;
+            MasMax = masMax;
+        }
+        public void MasRandom()
+        {
+            Random rnd = new Random();
+            mas = new int[MasRow];
+            for (int i = 0; i < MasRow; i++)
+            {
+                mas[i] = rnd.Next(MasMax);
+            }
+        }
+        public int AvgMasSum()
+        {
+            MasRandom();
+            int masSum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                masSum += mas[i];
+            }
+            int masAvgSum = masSum / mas.Length;
+            int minIndex = -1;
+            double minValue = double.MaxValue;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                double difference = Math.Abs(mas[i] - masAvgSum);
+
+                if (difference < minValue)
+                {
+                    minValue = difference;
+                    minIndex = i;
+                }
+            }
+            return minIndex;
+        }
+    }
+    public struct TaskFour
+    {
+        int[,] matrix;
+        private int MatrixMax { get; set; }
+        private int MatrixRow { get; set; }
+        private int MatrixColumn { get; set; }
+        public TaskFour(int matrixMax, int matrixColumn, int matrixRow)
+        {
+            MatrixColumn = matrixColumn;
+            MatrixMax = matrixMax;
+            MatrixRow = matrixRow;
+        }
+        public void MatrixRandomAndCalcPasitiveNumber(out int[] masResult)
+        {
+            List<int> result = new List<int>();
+            Random rnd = new Random();
+            matrix = new int[MatrixRow, MatrixColumn];
+            for (int i = 0; i < MatrixRow; i++)
+            {
+                for (int j = 0; j < MatrixColumn; j++)
+                {
+                    matrix[i, j] = rnd.Next(-10, MatrixMax);
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0);i++)
+            {
+                for (int j = 0;j < matrix.GetLength(0);j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        result.Add(matrix[i, j]);
+                    }
+                }
+            }
+            masResult = new int[result.Count];
+            for (int i = 0;i < result.Count;i++)
+            {
+                masResult[i] = result[i];
+            }
+        }
+    }
+}
---
+++ ZadanieDlaPractica/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task;
+
+namespace ZadanieDlaPractica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var s = sender as Button;
+            switch(s.Name)
+            {
+                case "btTaskOne":TaskOneCalc(); break;
+                case "btTaskTwo":TaskTwoCalc(); break;
+                case "btTaskThree":TaskThreeCalc(); break;
+                case "btTaskFour":TaskFourCalc(); break;
+                case "btTaskInfo":MessageBox.Show("Made In Pasha"); break;
+                case "btTaskExit":Close(); break;
+            }
+        }
+        private void TaskOneCalc()
+        {
+            bool boolTaskOneThreeDigitNumber = int.TryParse(tbTaskOneThreeDigitNumber.Text, out int threeDigitNumber);
+            if (boolTaskOneThreeDigitNumber && threeDigitNumber > 9 && threeDigitNumber < 1000)
+            {
+                TaskOne taskOne = new TaskOne(threeDigitNumber);
+                bool boolTaskOneResult = taskOne.ResultTaskOne();
+                if (boolTaskOneResult) MessageBox.Show("Сумма явдяется двухзначной");
+                else MessageBox.Show("Сумма не является двухзначной");
+            }
+            else MessageBox.Show("Что-то не то");
+        }
+        private void TaskTwoCalc()
+        {
+            bool boolnumberOne = int.TryParse(tbTaskTwoNumberOne.Text, out int numberOne);
+            bool boolnumberTwo = int.TryParse(tbTaskTwoNumberTwo.Text, out int numberTwo);
+            bool boolnumberThree = int.TryParse(tbTaskTwoNumberThree.Text, out int numberThree);
+            if (boolnumberOne && boolnumberTwo && boolnumberThree)
+            {
+                TaskTwo taskTwo = new TaskTwo(numberOne, numberTwo, numberThree);
+                int MaxNumberResult = taskTwo.MaxNumber();
+                tbTaskTwoResult.Text = Convert.ToString(MaxNumberResult);
+            }
+            else MessageBox.Show("Что-то не то");
+        }
+        private void TaskThreeCalc()
+        {
+            bool boolMaxMas = int.TryParse(tbTaskThreeMaxMas.Text, out int maxMas);
+            bool boolMasRow = int.TryParse(tbTaskThreeMasRow.Text, out int masRow);
+            if (boolMasRow && boolMaxMas)
+            {
+                TaskThree taskThree = new TaskThree(maxMas, masRow);
+                int masResult = taskThree.AvgMasSum();
+                tbTaskThreeResult.Text = Convert.ToString(masResult);
+            }
+            else MessageBox.Show("Что-то не то");
+        }
+        private void TaskFourCalc()
+        {
+            bool boolMatrixMax = int.TryParse(tbTaskFourMaxMatrix.Text, out int matrixMax);
+            bool boolMatrixColumn = int.TryParse(tbTaskFourColumn.Text, out int matrixColumn);
+            bool boolMatrixRow = int.TryParse(tbTaskFourRow.Text, out int matrixRow);
+            if(boolMatrixColumn && boolMatrixMax && boolMatrixRow)
+            {
+                int[] mas;
+                TaskFour taskFour = new TaskFour(matrixMax, matrixColumn, matrixRow);
+                taskFour.MatrixRandomAndCalcPasitiveNumber(out mas);
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    lbMasResult.Items.Add(mas[i]);
+                }
+            }
+            else MessageBox.Show("Что-то не то");
+        }
+    }
+}
